3f8c21b32df1f197bdd45829629bb6e66ed7b21d
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-03-05 17:30:23 +0200

    Add public and views directories

--- views/templates/adminNav.php
+++ views/templates/adminNav.php
@@ -4,15 +4,15 @@ include "head.php";
 
 <!-- Navbar start -->
 <div class="container-fluid p-0 nav-bar" style="background-color: #362517; ">
-  <nav class="navbar navbar-expand-lg navbar-dark py-3 fs-3 d-flex justify-content-between">
+  <nav class="navbar navbar-expand-lg navbar-dark py-3 fs-4 d-flex justify-content-sm-evenly">
     <a class="navbar-brand px-lg-4 me-5">
-      <h1 class="m-0 display-4 text-white">ITI Cafeteria</h1>
+      <h1 class="m-0 display-4 fs-1 text-white">ITI Cafeteria</h1>
     </a>
     <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-      <div class="navbar-nav px-md-5">
+      <div class="navbar-nav px-5">
         <a href="/admin-home" class="nav-item nav-link mx-3 ">Home</a>
         <a href="/admin-products" class="nav-item nav-link mx-3">Products</a>
         <a href="/admin-users" class="nav-item nav-link mx-3">Users</a>
