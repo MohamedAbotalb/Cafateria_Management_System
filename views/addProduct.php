459742f122fdf4fac0e07d00e64adebc857e7e6b
naglaasaad2000 <naglaasaadmohamed@gmail.com>
2024-03-06 18:19:21 +0200

    updat

---
+++ views/addProduct.php
@@ -0,0 +1,38 @@
+<?php
+require_once "templates/adminNav.php";
+?>
+
+<div class="container my-5">
+  <h1>Add Product</h1>
+  <form class="my-5" action="" method="" enctype="multipart/form-data">
+    <div class="mb-3">
+      <label for="productName" class="form-label">Product</label>
+      <input type="text" class="form-control" id="productName" placeholder="Enter product name">
+    </div>
+    <div class="mb-3">
+      <label for="productPrice" class="form-label">Price</label>
+      <input type="text" class="form-control" id="productPrice" placeholder="Enter product price">
+    </div>
+    <div class="mb-3">
+      <label for="productCategory" class="form-label">Category</label>
+      <select class="form-select" id="productCategory">
+        <option selected>Select category</option>
+        <option value="electronics">Electronics</option>
+        <option value="clothing">Clothing</option>
+        <option value="home">Home</option>
+        <option value="beauty">Beauty</option>
+      </select>
+      <button type="button" class="btn btn-primary mt-2">Add Category</button>
+    </div>
+    <div class="mb-3">
+      <label for="productImage" class="form-label">Product Image</label>
+      <input type="file" class="form-control" id="productImage">
+    </div>
+    <div class="mb-3">
+      <button type="submit" class="btn button">Save</button>
+      <button type="reset" class="btn btn-secondary">Reset</button>
+    </div>
+  </form>
+</div>
+
+
