a834346fc2675c79039ec02440ec7c1726bf8cd4
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-03-07 08:12:29 +0200

    Update addUser.php

--- views/addUser.php
+++ views/addUser.php
@@ -74,26 +74,3 @@ require_once "templates/adminNav.php";
     </div>
   </div>
 </div>
-
-<script>
-  // Custom JavaScript validation
-  (function () {
-    'use strict';
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation');
-
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-
-          form.classList.add('was-validated');
-        }, false);
-      });
-  })();
-</script>
