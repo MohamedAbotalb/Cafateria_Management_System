160967e80b03de051504b601810bba50ee29c59f
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-03-07 08:13:00 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -79,4 +79,20 @@ window.onload = function () {
 
       });
     });
+
+    (function () {
+    var forms = document.querySelectorAll('.needs-validation');
+
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+
+          form.classList.add('was-validated');
+        }, false);
+      });
+  })();
 };
