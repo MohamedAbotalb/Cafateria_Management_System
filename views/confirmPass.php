88c3aac7e236605f7cae8fe371abc2bf1acd2279
Ahmed Ebrahem <ai909940@gmail.com>
2024-03-09 18:37:35 +0200

    Update login and confirm pages

--- views/confirmPass.php
+++ views/confirmPass.php
@@ -4,12 +4,12 @@ require_once "templates/adminNav.php";
 
 <div class="container my-5">
     <h1 class="text-center">Cafeteria</h1>
-    <form action="#" class="w-50 m-auto text-bg-light my-5 p-3 rounded shadow-lg bg-body-tertiary" novalidate>
+    <form method="" action="#" class="w-50 m-auto text-bg-light my-5 p-3 rounded shadow-lg bg-body-tertiary" novalidate>
         <div class="row g-3 align-items-center my-3">
             <div class="col-5">
                 <label for="inputPassword" class="col-form-label">Enter New Password</label>
             </div>
-            <div class="col-6">
+            <div class="col-7">
                 <input type="password" id="inputPassword" class="form-control" aria-describedby="passwordHelpInline" pattern=".{6,}" title="Password must contain at least 3 characters" required>
                 <div class="invalid-feedback">Please enter a valid password with at least 6 characters.</div>
             </div>
@@ -18,7 +18,7 @@ require_once "templates/adminNav.php";
             <div class="col-5">
                 <label for="confirmPassword" class="col-form-label">Confirm New Password</label>
             </div>
-            <div class="col-6">
+            <div class="col-7">
                 <input type="password" id="confirmPassword" class="form-control" aria-describedby="passwordHelpInline" required>
                 <div class="invalid-feedback">Passwords do not match.</div>
             </div>
