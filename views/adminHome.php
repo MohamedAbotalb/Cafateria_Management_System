ddd91e3102b1fc06fa570f0aac40b932a5d8fe14
Nada Alnajdi <nada.abdo.alnajdi@gmail.com>
2024-03-13 02:07:05 +0200

    search by product name in admin and user page

--- views/adminHome.php
+++ views/adminHome.php
@@ -1,8 +1,8 @@
 <?php
-session_start(); 
+session_start();
 require_once "templates/adminNav.php";
 require_once "../models/db.php";
-$db=new DB();
+$db = new DB();
 ?>
 
 <!-- start of search -->
@@ -11,12 +11,10 @@ $db=new DB();
     <div class="col-9"></div>
 
     <div class="col-3">
-      <form method="" action="">
-        <div class="input-group mb-1">
-          <input type="text" class="form-control" placeholder="Enter item" aria-label="Recipient's username" aria-describedby="button-addon2" />
-          <input type="submit" class="btn btn-outline-secondary" type="button" id="button-addon2" value="search" />
-        </div>
-      </form>
+      <div class="input-group mb-1">
+        <input id="searchInput" type="text" name="product" class="form-control rounded-pill" placeholder="Enter item"
+          aria-label="Recipient's username" aria-describedby="button-addon2" />
+      </div>
     </div>
 
   </div>
@@ -35,16 +33,17 @@ $db=new DB();
             <div class="list mx-3"></div>
             <!-- end of product order -->
             <form action="../controllers/addOrderController.php" method="post">
-            <input type="hidden" name="sourcePage" value="admin">
+              <input type="hidden" name="sourcePage" value="admin">
               <div class="form-floating my-3">
-                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" name="note"></textarea>
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"
+                  name="note"></textarea>
                 <label for="floatingTextarea">Notes</label>
               </div>
-              <select class="form-select " aria-label="Default select example" name='room' >
-                <option selected disabled >Select Room</option>
+              <select class="form-select " aria-label="Default select example" name='room'>
+                <option selected disabled>Select Room</option>
                 <?php
-                $rooms=$db->select("room");
-                foreach($rooms as $room){
+                $rooms = $db->select("room");
+                foreach ($rooms as $room) {
                   echo "<option value='{$room['id']}' >{$room['id']}</option>";
                 }
                 ?>
@@ -52,14 +51,14 @@ $db=new DB();
               <input type="hidden" name="productDetails" class="productDetails">
 
               <input type="hidden" name="invoicePrice" class="invoicePriceInput">
-              
+
               <hr class="my-4" />
               <p class="fw-bold"><span class="invoice-price">0</span> EGP</p>
               <?php
-                if ( isset($_SESSION['order_added']) && $_SESSION['order_added']) {
-                     echo '<div class="alert alert-success successAlert">Order send successfully</div>';
-                     $_SESSION['order_added'] = false;
-                }
+              if (isset($_SESSION['order_added']) && $_SESSION['order_added']) {
+                echo '<div class="alert alert-success successAlert">Order send successfully</div>';
+                $_SESSION['order_added'] = false;
+              }
               ?>
               <input type="submit" class="btn button" value="confirm" />
             </form>
@@ -71,19 +70,19 @@ $db=new DB();
       <div class="col-7 ">
         <h5 class="text-muted "> Add to user</h5>
         <select class="form-select w-50 my-5" aria-label="Default select example">
-        <?php
-                $users=$db->select("user",["role"],["user"]);
-                foreach($users as $user){
-                  echo "<option value='{$user['id']}'>{$user['name']}</option>";
-                }
-                ?>
+          <?php
+          $users = $db->select("user", ["role"], ["user"]);
+          foreach ($users as $user) {
+            echo "<option value='{$user['id']}'>{$user['name']}</option>";
+          }
+          ?>
         </select>
         <hr class="my-5" />
 
         <div class="section-title">
           <p class="display-5">Menu</p>
         </div>
-        <div class="d-flex flex-wrap">
+        <div id="product-card" class="d-flex flex-wrap">
           <?php
           $products = $db->select("product", ["available"], ["available"]);
           foreach ($products as $product) {
@@ -101,7 +100,56 @@ $db=new DB();
           }
           ?>
         </div>
-      <!-- end of menu -->
+        <!-- end of menu -->
+      </div>
     </div>
   </div>
-</div>
+
+  <script>
+    const searchInput = document.getElementById("searchInput");
+    const card = document.getElementById("product-card")
+
+    searchInput.addEventListener('keyup', function (e) {
+      e.preventDefault()
+
+      const productName = e.target.value.trim();
+
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          showProduct(this.responseText);
+
+        }
+      };
+      xmlhttp.open("GET", "../controllers/searchProductController.php?p=" + productName, true);
+      xmlhttp.send();
+
+
+    })
+
+    function showProduct(products) {
+
+      const productsArray = JSON.parse(products);
+
+      card.innerHTML = "";
+      if (Array.isArray(productsArray) && productsArray.length > 0) {
+        productsArray.forEach(product => {
+          card.innerHTML += `
+        <div class='card m-3 product' style='width: 9rem'>
+          <img src='../public/images/${product.image}' class='card-img-top' alt='...' />
+          <h5 class='menu-price'>$<span class='productPrice'>${product.price}</span></h5>
+          <div class='card-body'>
+            <p class='card-text'>
+              ${product.name}
+              <input type='hidden' class='productId' value='${product.id}'>
+            </p>
+          </div>
+        </div>`;
+        });
+      } else {
+        card.innerHTML = "<p>No products found.</p>";
+      }
+    }
+
+
+  </script>
--- views/userHome.php
+++ views/userHome.php
@@ -1,5 +1,5 @@
 <?php
-session_start(); 
+session_start();
 require_once "templates/userNav.php";
 require_once "../models/db.php";
 require_once "../models/homePage.php";
@@ -13,13 +13,12 @@ $homePage = new HomePage();
   <div class="row">
     <div class="col-9"></div>
     <div class="col-3">
-      <form method="" action="">
-        <div class="input-group mb-1">
-          <input type="text" class="form-control" placeholder="Enter item" aria-label="Recipient's username"
-            aria-describedby="button-addon2" />
-          <input type="submit" class="btn btn-outline-secondary" type="button" id="button-addon2" value="search" />
-        </div>
-      </form>
+
+      <div class="input-group mb-1">
+        <input id="searchInput" type="text" name="product" class="form-control rounded-pill"
+          placeholder="Enter item" aria-label="Recipient's username" aria-describedby="button-addon2" />
+      </div>
+
     </div>
 
   </div>
@@ -38,9 +37,10 @@ $homePage = new HomePage();
             <div class="list mx-3"></div>
             <!-- end of product order -->
             <form method="post" action="../controllers/addOrderController.php">
-            <input type="hidden" name="sourcePage" value="user">
+              <input type="hidden" name="sourcePage" value="user">
               <div class="form-floating my-3">
-                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" name="note"></textarea>
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"
+                  name="note"></textarea>
                 <label for="floatingTextarea">Notes</label>
               </div>
               <select class="form-select" aria-label="Default select example" name="room">
@@ -57,10 +57,10 @@ $homePage = new HomePage();
               <hr class="my-4" />
               <p class="fw-bold"><span class="invoice-price">0</span> EGP</p>
               <?php
-                if (isset($_SESSION['order_added']) && $_SESSION['order_added']) {
-                     echo '<div class="alert alert-success successAlert">Order send successfully</div>';
-                     $_SESSION['order_added'] = false;
-                }
+              if (isset($_SESSION['order_added']) && $_SESSION['order_added']) {
+                echo '<div class="alert alert-success successAlert">Order send successfully</div>';
+                $_SESSION['order_added'] = false;
+              }
               ?>
               <input type="submit" class="btn button" value="confirm" />
             </form>
@@ -70,15 +70,15 @@ $homePage = new HomePage();
       <!-- end of order -->
       <!-- start of menu -->
       <div class="col-7 ">
-        
-          <?php
-          $query = $homePage->Innerjoin();
-          $result = $db->getConnection()->query($query);
-          if($result && $result->rowCount() > 0){
-            echo "<h5 class='text-muted '> latest order</h5>
+
+        <?php
+        $query = $homePage->Innerjoin();
+        $result = $db->getConnection()->query($query);
+        if ($result && $result->rowCount() > 0) {
+          echo "<h5 class='text-muted '> latest order</h5>
             <div class='d-flex flex-wrap'>";
-            foreach ($result as $row) {
-              echo "<div class='card m-3' style='width: 9rem'>
+          foreach ($result as $row) {
+            echo "<div class='card m-3' style='width: 9rem'>
                   <img src='../public/images/{$row['image']}' class='card-img-top' alt='...' />
                   <div class='card-body'>
                     <p class='card-text'>
@@ -88,18 +88,18 @@ $homePage = new HomePage();
                   </div>
 
                 </div>";
-            }
-            echo "</div>
-          <hr class='my-4' />";
           }
-          
-          
-          ?>
-        
+          echo "</div>
+          <hr class='my-4' />";
+        }
+
+
+        ?>
+
         <div class="section-title">
           <p class="display-5">Menu</p>
         </div>
-        <div class="d-flex flex-wrap">
+        <div id="product-card" class="d-flex flex-wrap">
           <?php
           $products = $db->select("product", ["available"], ["available"]);
           foreach ($products as $product) {
@@ -108,19 +108,64 @@ $homePage = new HomePage();
                 <h5 class='menu-price'>$<span class='productPrice'>{$product['price']}</span></h5>
                 <div class='card-body'>
                     <p class='card-text'>
-                        {$product['name']}
+                    {$product['name']}
                         <input type='hidden' class='productId' value='{$product['id']}'>
-
+                        
                     </p>
-                </div>
+                    </div>
             </div>";
           }
           ?>
         </div>
-      <!-- end of menu -->
+        <!-- end of menu -->
+      </div>
     </div>
   </div>
-</div>
-<script>
-   
-</script>
+  <script>
+    const searchInput = document.getElementById("searchInput");
+    const card = document.getElementById("product-card")
+
+    searchInput.addEventListener('keyup', function (e) {
+      e.preventDefault()
+
+      const productName = e.target.value.trim();
+
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          showProduct(this.responseText);
+
+        }
+      };
+      xmlhttp.open("GET", "../controllers/searchProductController.php?p=" + productName, true);
+      xmlhttp.send();
+
+
+    })
+ 
+    function showProduct(products) {
+      
+      const productsArray = JSON.parse(products);
+
+      card.innerHTML = "";
+      if (Array.isArray(productsArray) && productsArray.length > 0) {
+        productsArray.forEach(product => {
+          card.innerHTML += `
+        <div class='card m-3 product' style='width: 9rem'>
+          <img src='../public/images/${product.image}' class='card-img-top' alt='...' />
+          <h5 class='menu-price'>$<span class='productPrice'>${product.price}</span></h5>
+          <div class='card-body'>
+            <p class='card-text'>
+              ${product.name}
+              <input type='hidden' class='productId' value='${product.id}'>
+            </p>
+          </div>
+        </div>`;
+        });
+      } else {
+        card.innerHTML = "<p>No products found.</p>";
+      }
+    }
+
+
+  </script>
