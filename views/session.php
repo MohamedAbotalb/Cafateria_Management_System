ff0adb9e433e9750e3868841027e5dec95470a43
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-03-14 13:18:54 +0200

    Remove session file from the views directory

--- views/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-$_SESSION["user_id"] = "31";
-$_SESSION["name"] = "doha";
-$_SESSION["email"] = "doha@doha.com";
-$_SESSION["password"] = "123";
-$_SESSION["image"] = "1.jpg";
-$_SESSION["room_id"] = "6";
